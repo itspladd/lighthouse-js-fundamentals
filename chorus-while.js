66a0c90c4c2eee86aacd90c392b62b1008bc2bfe
Paul Ladd <vlendle@gmail.com>
2021-02-06 14:30:32 +0000

    First draft of chorus-while, 10 loops

---
+++ chorus-while.js
@@ -0,0 +1,8 @@
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat <10) {
+  console.log(chorus);
+  repeat++;
+}
+
+console.log("Until the sun turns into a red giant and expands, consuming the Earth forever!");
