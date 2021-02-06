b999fb47c495e5e1548f2a18c3aea03ab558abe1
Paul Ladd <vlendle@gmail.com>
2021-02-06 23:00:16 +0000

    Improved error message clarity and added comments to output checking with the expected results.

--- age-calculator.js
+++ age-calculator.js
@@ -1,10 +1,14 @@
 function ageCalculator(name, yearOfBirth, currentYear) {
+  //Just one quick-and-dirty error check, in case someone gets the year inputs mixed up.
   if (yearOfBirth > currentYear) {
-    return "Error: Birth year is in the future!";
+    return `Error: Birth year (${yearOfBirth}) is in the future! You listed the current year as ${currentYear}.`;
   }
 
   return name + " is " + (currentYear - yearOfBirth) + " years old.";
 }
 
+//Should return "Miranda is 32 years old."
 console.log(ageCalculator("Miranda", 1983, 2015));
+
+//Should return "Ferdinand is 27 years old."
 console.log(ageCalculator("Ferdinand", 1988, 2015));
