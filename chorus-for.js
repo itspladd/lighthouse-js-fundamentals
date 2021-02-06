be03c3993ca6ae946af09090d221af96e0e97e71
Paul Ladd <vlendle@gmail.com>
2021-02-06 14:35:04 +0000

    New, more responsible version of the pop song, using a for loop.

---
+++ chorus-for.js
@@ -0,0 +1,6 @@
+const chorus = "Let's dance! Again!";
+for(let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+
+console.log("Until my \"Stop dancing and go to bed\" alarm goes off! Ah wait there it goes, gotta head home, bye");
