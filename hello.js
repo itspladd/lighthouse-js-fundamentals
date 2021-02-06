c438c8f9e5f48f22b36b2f1ad82695271bac63c5
Paul Ladd <vlendle@gmail.com>
2021-02-06 22:31:53 +0000

    Added a parameter to sayHello function

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
-const sayHello = function () {
-  console.log("Hello, world");
+const sayHello = function (name) {
+  console.log("Hello, " + name);
 }
 
-sayHello();
+sayHello("Roxy");
+sayHello("Dave");
+sayHello("Jade");
