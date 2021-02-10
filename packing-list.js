9c71df97b07e19ef498535bf2b6db71b9172b322
Paul Ladd <vlendle@gmail.com>
2021-02-10 04:01:09 +0000

    Added while loop iteration

--- packing-list.js
+++ packing-list.js
@@ -2,6 +2,15 @@ const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glas
 
 console.log("Kitchen stuff to pack:");
 
-for (let i = 0; i < packingList.length; i++) {
+//For loop iteration
+/* for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+} */
+
+//While loop iteration
+let i = 0;
+
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
